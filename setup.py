d7f2bba86b4cda46d75ace38819e0603c4ec9bf5
ceglia <amedeo.ceglia@umontreal.ca>
2022-02-24 09:24:09 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="OsimToBiomod",
+    description="Convert opensim model format into biomod model format.",
+    version='0.1',
+    author="Aceglia",
+    author_email="amedeo.ceglia@umontreal.ca",
+    url="https://github.com/aceglia/OsimToBiomod",
+    license="Apache 2.0",
+    packages=["OsimToBiomod"],
+    keywords="OsimToBiomod",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
