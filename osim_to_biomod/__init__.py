99284cad8b00f5f1b524e3a0ed5aa9fb92470a32
Pariterre <pariterre@hotmail.com>
2022-09-22 14:12:31 -0400

    Added appveyor tests

--- osim_to_biomod/__init__.py
+++ osim_to_biomod/__init__.py
@@ -1,5 +1,13 @@
 from .converter import Converter
 from .enums import (
-    MuscleType, MuscleStateType, JointType, ForceType, Controller, Constraint, ContactGeometry, Component, Probe
+    MuscleType,
+    MuscleStateType,
+    JointType,
+    ForceType,
+    Controller,
+    Constraint,
+    ContactGeometry,
+    Component,
+    Probe,
 )
-
+from .utils_stl import scale_stl, reduce_stl, stl_to_vtp  # Todo: decide if we should expose this or automatically do it when converting the file
