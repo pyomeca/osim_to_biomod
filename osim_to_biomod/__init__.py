ef3993bb4587538c77b733940e7f925c8109b99e
Pariterre <pariterre@hotmail.com>
2022-09-22 14:20:10 -0400

    Added check black github action

--- osim_to_biomod/__init__.py
+++ osim_to_biomod/__init__.py
@@ -10,4 +10,8 @@ from .enums import (
     Component,
     Probe,
 )
-from .utils_stl import scale_stl, reduce_stl, stl_to_vtp  # Todo: decide if we should expose this or automatically do it when converting the file
+from .utils_stl import (
+    scale_stl,
+    reduce_stl,
+    stl_to_vtp,
+)  # Todo: decide if we should expose this or automatically do it when converting the file
